e128298effc0024446e18ab7ff860ec37862f816
sivaprasadt123 <99263061+sivaprasadt123@users.noreply.github.com>
2022-02-08 20:11:41 +0530

    Create Imgjs.js

---
+++ RJTProject/ImageGalleryReactApp/Imgjs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+   return (
+     <div>
+       <center>
+           <form>
+              <input type = "text" /><br/><br/>
+              <input type = "submit" name = "SEARCH"/>
+          </form>  
+     </center>
+     </div>
+   )
+}
